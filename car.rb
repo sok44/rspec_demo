727e303294f3f51b40575ceea48d6a4b9f772449
Sok444 <sok_88@mail.ru>
2017-11-10 18:41:01 +0300

    Add car class

---
+++ car.rb
@@ -0,0 +1,26 @@
+class Car
+
+  #Сколько можем проехать на одном галоне
+  MILES_PER_GALLON = 20
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel = 0
+  end
+
+  def add_fuel amount
+    @fuel == amount
+  end
+
+  #Расчитываем как делеко мы сможем проехать
+  def range
+    0
+  end
+  
+end
+
+car = Car.new
+car.add_fuel 10
+
+puts "Range is #{car.range}"
