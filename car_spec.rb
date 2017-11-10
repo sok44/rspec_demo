72eebe5b58eb535f4bf59994a2b6a8b669f49d86
Sok444 <sok_88@mail.ru>
2017-11-10 18:53:48 +0300

    Add test for car

---
+++ car_spec.rb
@@ -0,0 +1,18 @@
+require './car.rb'
+
+describe Car do 
+
+  it "Must return range" do
+    
+    #arrange
+    car = Car.new
+
+    #act
+    car.add_fuel 10
+
+    #assert
+    expect(car.range).to eq 200
+
+  end
+  
+end
